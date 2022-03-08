75cbc7fe837c14792a7553a3031121495d33239d
Sajjad Alizadeh <sajjadalizadeh2000@gmail.com>
2022-03-08 03:09:32 -0500

    Add csv_to_node_list code

---
+++ tools/csv_to_node_list.py
@@ -0,0 +1,28 @@
+import sys
+import os
+import csv
+
+FROM_ADDRESS = 6
+TO_ADDRESS = 9
+
+if __name__ == "__main__":
+    filenames = os.listdir(sys.argv[1])
+    
+    addresses = set()
+
+    for filename in filenames:
+        reader = csv.reader(open(sys.argv[1] + '/' + filename, 'r'))
+        next(reader)
+        for row in reader:
+            addresses.add(row[FROM_ADDRESS])
+            addresses.add(row[TO_ADDRESS])
+
+        print("READ ", filename)
+
+    print(len(addresses))
+
+    f = open('nodes.csv', 'w')
+    writer = csv.writer(f)
+    for address in addresses:
+        writer.writerow(addresses)
+    f.close()
