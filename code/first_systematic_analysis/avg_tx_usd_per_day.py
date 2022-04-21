d8745a1acf59efbb36823df27c46898d5f6c9738
aminst <aminst79@gmail.com>
2022-04-21 18:35:18 +0430

    Add average tx usd per day

---
+++ code/first_systematic_analysis/avg_tx_usd_per_day.py
@@ -0,0 +1,45 @@
+import sys
+import os
+import csv
+import json
+import pandas as pd
+
+TX_VALUE_INDEX = 14
+
+def write_volume_to_json(filename, volume):
+    with open(filename, 'w') as json_file:
+        json_file.write(json.dumps(volume))
+
+def get_USD_exchange_value(timestamp, price_df):
+    matched_data = price_df.loc[price_df['date'] == timestamp]
+    if len(matched_data):
+        matched_data = matched_data.iloc[0]
+        return matched_data['open']
+    else:
+        return 1
+
+if __name__ == "__main__": # usage: python avg_tx_usd_per_day.py <path_to_csv_day_files> <usd_file>
+    filenames = os.listdir(sys.argv[1])
+    usd_file = sys.argv[2]
+
+    price_df = pd.read_csv(usd_file)
+
+    usd_per_date = dict()
+
+    for filename in filenames:
+        print("READ ", filename)
+        f = open(sys.argv[1] + '/' + filename, 'r')
+        reader = csv.reader(f)
+        time = filename.split('.')[0]
+        usd_per_date[time] = 0
+        usd = get_USD_exchange_value(time, price_df)
+        count = 0
+        for row in reader:
+            value = float(row[TX_VALUE_INDEX]) if row[TX_VALUE_INDEX] != 'null' else 0
+            usd_per_date[time] += value * usd
+            count += 1
+        print(count)
+        usd_per_date[time] /= count
+        print("DONE ", filename)
+    
+    write_volume_to_json('results/avg_tx_usd_per_day.json', usd_per_date)
