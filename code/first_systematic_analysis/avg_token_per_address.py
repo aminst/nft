1143c8e6c97fe29615cf037e2e7f92875fc0778b
aminst <aminst79@gmail.com>
2022-04-21 20:11:17 +0430

    Fix average token per address problems

--- code/first_systematic_analysis/avg_token_per_address.py
+++ code/first_systematic_analysis/avg_token_per_address.py
@@ -5,8 +5,6 @@ import json
 
 FROM_IDX = 5
 TO_IDX = 8
-TOKEN_ADDRESS_IDX = 9
-TOKEN_ID_IDX = 10
 
 def write_avg_token_to_json(filename, avg_token):
     with open(filename, 'w') as json_file:
@@ -14,13 +12,15 @@ def write_avg_token_to_json(filename, avg_token):
 
 if __name__ == "__main__":
     filenames = os.listdir(sys.argv[1])
+    sorted_filenames = sorted(filenames)
+    print(sorted_filenames)
 
-    nfts = set()
+    addresses = set()
 
     avg_zero_out_in_diff = dict()
 
     prev = 0
-    for filename in filenames:
+    for filename in sorted_filenames:
         print("READ ", filename)
         reader = csv.reader(open(sys.argv[1] + '/' + filename, 'r'))
         time = filename.split('.')[0]
@@ -29,8 +29,14 @@ if __name__ == "__main__":
                 prev += 1
             elif row[TO_IDX] == '0x0000000000000000000000000000000000000000':
                 prev -= 1
-            nfts.add(row[TOKEN_ADDRESS_IDX] + ':' + row[TOKEN_ID_IDX])
-        avg_zero_out_in_diff[time] = prev / len(nfts)
+            from_address = row[FROM_IDX]
+            to_address = row[TO_IDX]
+            if from_address != '0x0000000000000000000000000000000000000000':
+                addresses.add(from_address)
+            if to_address != '0x0000000000000000000000000000000000000000':
+                addresses.add(to_address)
+            
+        avg_zero_out_in_diff[time] = prev / len(addresses)
         print("DONE ", filename)
     
     write_avg_token_to_json('results/avg_token_per_address.json', avg_zero_out_in_diff)
